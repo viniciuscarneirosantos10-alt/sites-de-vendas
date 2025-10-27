46d0f91c50726a516b28790c8a87f3bdecd38c57
viniciuscarneirosantos10-alt <vinicius.carneiro.santos10@escola.pr.gov.br>
2025-10-27 09:53:35 -0300

    .

--- script.js
+++ script.js
@@ -12,8 +12,8 @@
         {
             name: 'Kawasaki Ninja 400',
             price: 'R$ 35.810,00',
-            image: 'kawasaki-ninja-400
-        },
+            image:`https://www.kawasakibrasil.com/content/dam/products/pim/studio/Resource_312313_25ZX400S_141GN1DRF3CG_A.jpg
+        }
         {
             name: 'Honda CB 1000R',
             price: 'R$ 78.870,00',
@@ -21,18 +21,18 @@
         },
         {
             name: 'BMW R1250 GS',
-            price: 'R$ 96.500,00',
-            image: 'https://images.pexels.com/photos/1413412/pexels-photo-1413412.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            price: 'R$ 96.50'
+            image: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAfzJuSpuN3LfAMiHkKSbIXbTeAnXT7BK0yg&s
         },
         {
-            name: 'Harley-Davidson Fat Boy',
+            name: 'BMW R1200,
             price: 'R$ 119.900,00',
-            image: 'https://images.pexels.com/photos/2530590/pexels-photo-2530590.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            image:'https://http2.mlstatic.com/D_778161-MLB91040428835_082025-C.jpg
         },
         {
             name: 'Ducati Panigale V4',
             price: 'R$ 162.990,00',
-            image: 'https://images.pexels.com/photos/3513238/pexels-photo-3513238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            image: 'https://production.autoforce.com/uploads/picture/image/258935247/comprar-panigale-v4-s-2023-2281bd40-83c1-4bca-92a1-6106d9e01aeb_22a0b74550.jpg
         }
     ];
 
